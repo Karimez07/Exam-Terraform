a3f4d00b735065f586325e833b4fb48858a8b3ff
Sofian <Sofian.ezahery0@gmail.com>
2026-08-24 16:26:33 +0000

    up

--- modules/ec2/variables.tf
+++ modules/ec2/variables.tf
@@ -2,10 +2,13 @@
 variable "namespace" {
   type = string
 }
-  variable "public_subnet_id" {
-    type = string
+variable "public_subnet_id" {
+  type = string
 }
 
-  variable "web_security_group_id" {
+variable "web_security_group_id" {
+  type = string
+}
+output "database_security_group_id" {
     type = string
- }
+  }
