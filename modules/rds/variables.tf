115884247a7b60ada45d951469b3cd756fd64172
Sofian <Sofian.ezahery0@gmail.com>
2026-08-24 19:45:25 +0000

    update

---
+++ modules/rds/variables.tf
@@ -0,0 +1,19 @@
+variable "namespace" {
+  type = string
+}
+
+variable "db_username" {
+  type = string
+}
+
+variable "db_password" {
+  type      = string
+  sensitive = true
+}
+
+variable "database_subnet_group_name" {
+  type = string
+}
+variable "database_security_group_id" {
+  type = string
+}
