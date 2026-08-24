c428074523c4f7e59e782416c88206bb4c727a7e
Sofian <Sofian.ezahery0@gmail.com>
2026-08-24 20:18:04 +0000

    update

--- modules/ec2/outputs.tf
+++ modules/ec2/outputs.tf
@@ -2,3 +2,10 @@
 output "public_ip" {
   value = aws_instance.ec2_public.public_ip
 }
+output "instance_id" {
+  value = aws_instance.ec2_public.id
+}
+
+output "availability_zone" {
+  value = aws_instance.ec2_public.availability_zone
+}
